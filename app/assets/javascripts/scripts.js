884c6d2060fcd4821d09dcb95cc75fa3ed1b3a47
Russell Thorn <1692222+russellthorn@users.noreply.github.com>
2020-04-22 16:17:29 +0100

    Hoss 125 (#25)
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614591679
    
    - error messages are now sentence-case
    - updated h1 to use smart quotes
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614595120
    
    - Removed js injecting aria-invalid to the fields, the js wasn't working as expected
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614598015
    
    Associate error messages with the appropriate inputs by using an aria-describedby attribute on the input.
    - update input helper with logic to show aria-describedby for inputs with hints and additionaly for inputs with errors
    - update view and messages
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614599595
    
    Move the form hint and error message to outside the legend for the date fieldset
    
    * Update date input type and add inputmode=numeric
    
    This addresses 2 comments
    Update date input type and add inputmode=numeric
    https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614601188
    Date input error styling is incorrect
    https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-615281804
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614602494
    
    Remove role="tooltip" from error messages
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614604840
    
    Prefix error messages with visually hidden Error: for screen reader users
    
    * Update status found page title to better match the h1 but without displaying personal data
    
    https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614606196
    
    After discussing with James Watson, the team's content designer, we decided that the generic dynamic title - Applicant has X status would be more appropriate
    
    - update message content
    - add dynamic status to title
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614608642
    
    - add change link to each line and add specific hidden text
    - add space in
    - update camel case
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614609513
    
    re-structure sentence so link is at the end of the sentence - less fragmented for screen reader users.
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614610178
    
    add missing -'s
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614785902
    
    reordering palette and focus fixes minor incorrect button focus style
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-615279421
    
    Update input error focus to match the the design system
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614590010
    
    Give error summary focus on page load when form displays errors
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614607208
    
    Apply the polyfill to allow the spacebar to action the button
    
    * https://github.com/hmrc/accessibility-audits-internal/issues/30#issuecomment-614599595
    
    I forgot to aria-describedby to the date fieldset

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,18 +1,10 @@
+GOVUK.shimLinksWithButtonRole.init()
+
 $(function() {
 
     //Accessibility
     var errorSummary =  $('#error-summary-display'),
         $input = $('input:text');
-
-    //Error summary focus
-    if (errorSummary){ errorSummary.focus() }
-    $input.each( function(){
-        if($(this).closest('label').hasClass('form-field--error')){
-            $(this).attr('aria-invalid', true)
-        }else{
-            $(this).attr('aria-invalid', false)
-        }
-    });
 
     //Trim inputs and Capitalize postode
     $('[type="submit"]').click(function(){
@@ -24,6 +16,13 @@ $(function() {
             }
         });
     });
+
+    //======================================================
+    // Move immediate focus to any error summary
+    //======================================================
+    if ($('.error-summary a').length > 0) {
+        $('.error-summary').focus();
+    }
 
     //Add aria-hidden to hidden inputs
     $('[type="hidden"]').attr("aria-hidden", true);
