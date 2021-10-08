4db1a00b416e9c9cf872dd4a52c46f63824eab61
Adam Cooper <61467301+AdamCoops@users.noreply.github.com>
2021-10-08 09:28:34 +0100

    HOSS2-135-Update Conflicts again

--- app/uk/gov/hmrc/homeofficeimmigrationstatus/views/StatusFoundPageContext.scala
+++ app/uk/gov/hmrc/homeofficeimmigrationstatus/views/StatusFoundPageContext.scala
@@ -29,14 +29,7 @@ final case class StatusFoundPageContext(
   val mostRecentStatus: Option[ImmigrationStatus] = result.mostRecentStatus
   val previousStatuses: Seq[ImmigrationStatus] = result.previousStatuses
 
-  val hasImmigrationStatus: Boolean = mostRecentStatus.map(_.productType).contains(EUS) &&
-    mostRecentStatus.map(_.immigrationStatus).exists(ImmigrationStatus.settledStatusSet.contains)
-
   val displayRecourseToPublicFunds: Boolean = mostRecentStatus.exists(_.noRecourseToPublicFunds)
-
-  val hasExpiredImmigrationStatus: Boolean = hasImmigrationStatus && mostRecentStatus.exists(_.expiredMsg.nonEmpty)
-
-  val statusClass: String = if (hasImmigrationStatus) "success" else "error"
 
   def currentStatusLabel(implicit messages: Messages): String = {
     val prefix = "status-found.current."
