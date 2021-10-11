fea01d2e72aff48a43976d93d7e87f845f9f0e10
Liam Oldfield <liamoldfield@hotmail.co.uk>
2021-10-11 17:58:04 +0100

    spec for sumary list

--- test/uk/gov/hmrc/homeofficeimmigrationstatus/views/ViewSpec.scala
+++ test/uk/gov/hmrc/homeofficeimmigrationstatus/views/ViewSpec.scala
@@ -58,6 +58,9 @@ trait ViewSpec extends PlaySpec with GuiceOneAppPerSuite with Injecting {
   def assertRenderedById(doc: Document, id: String) =
     assert(doc.getElementById(id) != null, "\n\nElement " + id + " was not rendered on the page.\n")
 
+  def assertRenderedByCssSelector(doc: Document, cssSelector: String) =
+    assert(doc.select(cssSelector) != null, "\n\nElement " + cssSelector + " was not rendered on the page.\n")
+
   def assertNotRenderedById(doc: Document, id: String) =
     assert(doc.getElementById(id) == null, "\n\nElement " + id + " was rendered on the page.\n")
 
