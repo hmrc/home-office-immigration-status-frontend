21c66e3765df487b6958df67185fa3ff70fb2ace
nboaram <14924378+Nboaram@users.noreply.github.com>
2023-02-14 14:26:21 +0000

    pr bot comments other than crypto

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,9 +2,11 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "7.13.0"
+
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "7.13.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "6.4.0-play-28",
     "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
     "uk.gov.hmrc"       %% "domain"                     % "8.1.0-play-28",
@@ -13,13 +15,14 @@ object AppDependencies {
   )
 
   private val test: Seq[ModuleID] = Seq(
-    "org.scalatest"                %% "scalatest"            % "3.2.15",
-    "org.mockito"                   % "mockito-core"         % "4.11.0",
-    "org.scalatestplus"            %% "scalacheck-1-17"      % "3.2.15.0",
-    "org.scalatestplus.play"       %% "scalatestplus-play"   % "5.1.0",
-    "com.github.tomakehurst"        % "wiremock-jre8"        % "2.35.0",
-    "com.vladsch.flexmark"          % "flexmark-all"         % "0.62.2",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.14.2"
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapVersion,
+    "org.scalatest"                %% "scalatest"                  % "3.2.15",
+    "org.mockito"                   % "mockito-core"               % "4.11.0",
+    "org.scalatestplus"            %% "scalacheck-1-17"            % "3.2.15.0",
+    "org.scalatestplus.play"       %% "scalatestplus-play"         % "5.1.0",
+    "com.github.tomakehurst"        % "wiremock-jre8"              % "2.35.0",
+    "com.vladsch.flexmark"          % "flexmark-all"               % "0.62.2",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.14.2"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
