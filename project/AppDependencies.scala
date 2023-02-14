4fe5a0091d5fd67b44127ff8906a22cb2aa3d0df
nboaram <14924378+Nboaram@users.noreply.github.com>
2023-02-14 14:41:01 +0000

    bootstrap TEST

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,14 +15,14 @@ object AppDependencies {
   )
 
   private val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "org.scalatest"                %% "scalatest"                  % "3.2.15",
-    "org.mockito"                   % "mockito-core"               % "4.11.0",
-    "org.scalatestplus"            %% "scalacheck-1-17"            % "3.2.15.0",
-    "org.scalatestplus.play"       %% "scalatestplus-play"         % "5.1.0",
-    "com.github.tomakehurst"        % "wiremock-jre8"              % "2.35.0",
-    "com.vladsch.flexmark"          % "flexmark-all"               % "0.62.2",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.14.2"
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % bootstrapVersion,
+    "org.scalatest"                %% "scalatest"              % "3.2.15",
+    "org.mockito"                   % "mockito-core"           % "4.11.0",
+    "org.scalatestplus"            %% "scalacheck-1-17"        % "3.2.15.0",
+    "org.scalatestplus.play"       %% "scalatestplus-play"     % "5.1.0",
+    "com.github.tomakehurst"        % "wiremock-jre8"          % "2.35.0",
+    "com.vladsch.flexmark"          % "flexmark-all"           % "0.62.2",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.14.2"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
