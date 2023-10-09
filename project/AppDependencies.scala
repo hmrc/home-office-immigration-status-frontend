133c2dbce0e06f1429693c6ab1b7133d8a777f42
Gaston Racui <gastonracui@gmail.com>
2023-10-09 09:42:14 +0100

    DDCE-4659 Add missing gtm container key

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.21.0"
+  private val bootstrapVersion = "7.22.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.22.0-play-28",
     "uk.gov.hmrc"       %% "play-partials"              % "8.4.0-play-28",
     "uk.gov.hmrc"       %% "domain"                     % "8.3.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % "1.3.0",
@@ -16,9 +16,9 @@ object AppDependencies {
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"  % bootstrapVersion,
-    "org.scalatest"                %% "scalatest"               % "3.2.16",
-    "org.mockito"                  %% "mockito-scala-scalatest" % "1.17.14",
-    "org.scalatestplus"            %% "scalacheck-1-17"         % "3.2.16.0",
+    "org.scalatest"                %% "scalatest"               % "3.2.17",
+    "org.mockito"                  %% "mockito-scala-scalatest" % "1.17.27",
+    "org.scalatestplus"            %% "scalacheck-1-17"         % "3.2.17.0",
     "com.github.tomakehurst"        % "wiremock-jre8"           % "2.35.0",
     "com.vladsch.flexmark"          % "flexmark-all"            % "0.64.8",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"    % "2.15.2"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -7,7 +7,7 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefact
 // Try to remove when sbt 1.8.0+ and scoverage is 2.0.7+
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
 
-addSbtPlugin("uk.gov.hmrc"         % "sbt-auto-build"           % "3.9.0")
+addSbtPlugin("uk.gov.hmrc"         % "sbt-auto-build"           % "3.14.0")
 addSbtPlugin("com.typesafe.play"   % "sbt-plugin"               % "2.8.20")
 addSbtPlugin("uk.gov.hmrc"         % "sbt-distributables"       % "2.2.0")
 addSbtPlugin("org.scoverage"       % "sbt-scoverage"            % "2.0.8")
