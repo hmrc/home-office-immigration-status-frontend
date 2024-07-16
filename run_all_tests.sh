fdd03959330a94584b35576bb62238e3adb43dc9
mywyau <47248104+mywyau@users.noreply.github.com>
2024-07-16 13:46:35 +0100

    remove scalastyle

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt scalafmtAll scalastyleAll clean coverage compile test it/test A11y/test coverageOff dependencyUpdates coverageReport
+sbt scalafmtAll clean coverage compile test it/test A11y/test coverageOff dependencyUpdates coverageReport
