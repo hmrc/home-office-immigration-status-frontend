73b4711e6cc8b2eca9808c9b0cc18529beba40ad
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2025-07-03 11:05:28 +0100

    added accessible autocomplete to docType

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.6.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.7.0",
     "uk.gov.hmrc"       %% "domain-play-30"             % "12.1.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.2.0"
