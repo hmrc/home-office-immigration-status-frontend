833cf776af502e4051bc8de48062e09f111b5729
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2025-08-31 21:46:25 +0100

    DDCE-6857 - Upgrade versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoVersion = "2.6.0"
+  private val hmrcMongoVersion = "2.7.0"
   private val bootstrapVersion = "10.1.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.7.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.8.0",
     "uk.gov.hmrc"       %% "domain-play-30"             % "12.1.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.2.0"
