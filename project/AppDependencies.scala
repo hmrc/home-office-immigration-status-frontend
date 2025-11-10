05f200f3f5758324d10b9886bf42351f17b0a5f0
grantrobson <grant.robson@digital.hmrc.gov.uk>
2025-11-10 07:17:10 +0000

    DLSN-220 More libs updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.19.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.20.0",
     "uk.gov.hmrc"       %% "domain-play-30"             % "12.1.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.4.0"
