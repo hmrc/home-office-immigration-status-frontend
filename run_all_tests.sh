1ba9db4b42136ac23affb8ea5405567c32315882
davidfes <128475768+davidfes@users.noreply.github.com>
2025-11-17 16:18:19 +0000

    DLSN-170 Fix warnings

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-sbt scalafmtAll clean coverage compile test it/test coverageOff dependencyUpdates coverageReport
+sbt scalafmtAll clean coverage # Keep these separate, the optimisations break the compilation
+sbt compile test it/test coverageOff dependencyUpdates coverageReport
