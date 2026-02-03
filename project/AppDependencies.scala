28d0fdff5de743e64867e833c9fee0a82b809ab1
Monisha.Radjou24 <195295576+MonishaRadjou24@users.noreply.github.com>
2026-02-03 14:41:58 +0000

    Update the mongo version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoVersion = "2.10.0"
-  private val bootstrapVersion = "10.4.0"
+  private val hmrcMongoVersion = "2.12.0"
+  private val bootstrapVersion = "10.5.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.21.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.29.0",
     "uk.gov.hmrc"       %% "domain-play-30"             % "12.1.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "crypto-json-play-30"        % "8.4.0"
