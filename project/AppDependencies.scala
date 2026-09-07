8562fd4c7aa7c73f48e742b6961e668a26339a06
grantrobson <grant.robson@digital.hmrc.gov.uk>
2026-09-07 07:47:41 +0100

    DLSN-459 Update libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoVersion = "2.12.0"
+  private val hmrcMongoVersion = "2.13.0"
   private val bootstrapVersion = "10.7.0"
 
   private val compile: Seq[ModuleID] = Seq(
